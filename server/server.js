754bafafd5a2790e6eb8e4e49265bafe6b7d7ac4
marysgreenwood <marysgreenwood@gmail.com>
2024-09-01 11:00:51 -0400

    added product post route

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ const PORT = 4000;
 const cors = require("cors");
 const mongoDBURL = require("./config");
 const mongoose = require("mongoose");
-
-
+const routes = require("./routes/productRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +14,8 @@ app.use(cors());
 app.get("/", cors(), async (req, res) => {
   res.send("Working!");
 });
+
+app.use("/products", routes);
 
 mongoose.connect(mongoDBURL).then(() => {
   console.log("App connected to database");

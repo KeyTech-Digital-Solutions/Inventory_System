2c8a06beafdbb05dea98c9dabc2a977b4f4aba2c
Mary Greenwood <marysgreenwood@gmail.com>
2024-09-26 19:46:35 -0400

    Revert "Mary"

--- server/config.js
+++
@@ -1,6 +0,0 @@
-// require("dotenv").config();
-
-
-const mongoDBURL = `mongodb+srv://root:60edVdaJb9nNpe9S@keytechinventory.tywfz.mongodb.net/?retryWrites=true&w=majority&appName=KeyTechInventory`;
-
-module.exports = mongoDBURL;

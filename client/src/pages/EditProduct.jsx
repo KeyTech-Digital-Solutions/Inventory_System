ad65f9ae2bcf35c881d6efc9b57ef5fa8c6e6c4a
marysgreenwood <marysgreenwood@gmail.com>
2024-10-15 12:29:18 -0400

    added basic components with http requests

---
+++ client/src/pages/EditProduct.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+
+export const EditProduct = () => {
+
+  return (
+    <div>
+      <div>
+        <label>Price</label>
+        <input type="text"></input>
+      </div>
+      <div>
+        <label>Cost</label>
+        <input></input>
+      </div>
+      <div>
+        <label>Quantity</label>
+        <input></input>
+      </div>
+    </div>
+  )
+}
--- server/routes/dataRoutes.js
+++ server/routes/dataRoutes.js
@@ -1,57 +1,75 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Data = require('../models/Data');
+const mongoose = require("mongoose");
+const Data = require("../models/Data");
 
-
-router.get('/', async (req, res) => {
+//GET ALL
+router.get("/", async (req, res) => {
   try {
-    const data = await Data.find(); 
+    const data = await Data.find();
     res.json(data);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
+//GET BY ID
+router.get("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const product = await Data.findById(id);
+    return response.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 // POST
-router.post('/products', (req, res) => {
-  const { name, productCode } = req.body;
+router.post("/products", (req, res) => {
+  const { name, productCode, cost, price, quantity } = req.body;
 
   const newProduct = new Data({
     name,
     productCode,
+    cost,
+    price,
+    quantity,
   });
 
   const savedProduct = newProduct.save();
 
   if (!name || !productCode) {
-      return res.status(400).json({ message: 'Name and Product Code are required' });
+    return res
+      .status(400)
+      .json({ message: "Name and Product Code are required" });
   }
 
-  res.status(201).json({ message: 'Data received', data: { name, productCode } });
+  res.status(201).json({
+    message: "Data received",
+    data: { name, productCode, cost, price, quantity },
+  });
 });
 
-
 // DELETE
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ message: 'Invalid ID' });
+    return res.status(400).json({ message: "Invalid ID" });
   }
 
   try {
     const deletedData = await Data.findByIdAndDelete(id);
 
     if (!deletedData) {
-      return res.status(404).json({ message: 'Data not found' });
+      return res.status(404).json({ message: "Data not found" });
     }
 
-    res.status(200).json({ message: 'Data deleted successfully', deletedData });
+    res.status(200).json({ message: "Data deleted successfully", deletedData });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
